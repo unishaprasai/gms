285e901df4a2e2238c6292926d2a7aa47767f934
Unisha Prasai <unishaprasai319@gmail.com>
2024-02-08 20:58:10 +0545

    first commit

---
+++ app/Http/Controllers/backend/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $usertype = Auth()->user()->usertype; // corrected syntax
+            if ($usertype == 'user') {
+                return view('dashboard');
+            }
+
+            if ($usertype == 'admin') {
+                return view('backend.home');
+            } else {
+                return redirect()->back();
+            }
+        }
+    }
+}
